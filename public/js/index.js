eeccc80cf52523367a70f9b72f7f7ec1c97b5ffb
Aldea <mihai.aldea@atos.net>
2018-12-14 10:50:06 +0200

    Addon emitting events

---
+++ public/js/index.js
@@ -0,0 +1,14 @@
+var socket = io();
+
+socket.on('connect', function (){
+    console.log('Connected to server');
+
+});
+
+socket.on('disconnect',function (){
+    console.log('Disconnected form server');
+});
+
+socket.on('newMessage', function(message) {
+    console.log('Got a new Message ', message);
+})
